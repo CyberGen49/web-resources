1fc5c295f0cbff60df44e090f5087beaf3fef048
CyberGen49 <38225790+CyberGen49@users.noreply.github.com>
2022-09-02 23:43:09 -0500

    Update base.js

--- base.js
+++ base.js
@@ -257,7 +257,7 @@ function isValidIp(string) {
 function escapeHTML(text) {
     let escape = document.createElement('span');
     escape.innerText = text;
-    return escape.innerText.replace('"', '&quot;');
+    return escape.innerText.replace(/"/g, '&quot;');
 }
 
 /**
